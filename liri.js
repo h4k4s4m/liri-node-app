41a4568a42ad204179e5653bcc45c40241967a80
Sahm Samarghandi <h4k4s4m@gmail.com>
2017-11-22 13:06:03 -0800

    Almost Done

--- liri.js
+++ liri.js
@@ -1,19 +1,22 @@
 //Twitter stuff
 const Twitter = require('twitter');
 const twitterKeys = require('./keys.js');
-var client = new Twitter(twitterKeys);
-var params = {
+const client = new Twitter(twitterKeys);
+const params = {
     screen_name: 'NotABot27847869'
 };
 
 //Spotify stuff
-var Spotify = require('node-spotify-api');
-var spotify = new Spotify({
+const Spotify = require('node-spotify-api');
+const spotify = new Spotify({
     id: "45e2b37138de4e858e457168edd7584e",
     secret: "073901c2915a474fbcdd79a99784a140"
 });
 
+//Used for OMDB
+const request = require('request');
 
+//General Vars
 var command = process.argv[2];
 
 switch (command) {
@@ -22,8 +25,13 @@ switch (command) {
         break;
 
     case "spotify-this-song":
-        //the 4th argument is the song name inputed by the user
+        //the 4th arguement is the song name inputed by the user
         spot(process.argv[3]);
+        break;
+
+    case "movie-this":
+        //the 4th arguement is the movie title to be searched
+        muvies(process.argv[3]);
         break;
 }
 
@@ -65,4 +73,33 @@ function spot(song) {
             console.log("Album: ")
             console.log(response.tracks.items[0].album.artists[0].album.name + '\n');
         });
+}
+
+function muvies(title) {
+
+    if (!title) {
+        title = "Mr. Nobody";
+    }
+
+    title = title.split(' ').join('+').replace('.', '');
+
+    request('http://www.omdbapi.com/?apikey=40e9cece&t=' + title, function (error, data, body) {
+        body = JSON.parse(body);
+        console.log("Title:", body.Title);
+        console.log("----------------------");
+        console.log("Year: ", body.Year);
+        console.log("----------------------");
+        console.log("IMDB Rating: ", body.Ratings[0].Value);
+        console.log("----------------------");
+        console.log("RT Rating: ", body.Ratings[1].Value);
+        console.log("----------------------");
+        console.log("Country: ", body.Country);
+        console.log("----------------------");
+        console.log("Language: ", body.Language);
+        console.log("----------------------");
+        console.log("Plot: ", body.Plot);
+        console.log("----------------------");
+        console.log("Actors: ", body.Actors);
+
+    });
 }
